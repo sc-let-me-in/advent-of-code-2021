1229d08171be802f41de1a1392a8b30385ba081f
William Reed <wreed58@gmail.com>
2021-12-02 11:09:33 -0500

    day 2, part 2

--- 02/w.clj
+++ 02/w.clj
@@ -15,7 +15,24 @@
           (= action "up")       (recur (rest moves) (- depth value) position)
           (= action "down")     (recur (rest moves) (+ depth value) position))))))
 
+(defn navigate-aim [input]
+  (loop [moves input
+         depth 0
+         aim 0
+         position 0]
+    (if (empty? moves) (* depth position)
+      (let [split-cmd (clojure.string/split (first moves) #" ")
+            action (first split-cmd)
+            value (Integer/parseInt (second split-cmd))]
+        (cond
+          (= action "forward")  (recur (rest moves) (+ depth (* aim value)) aim           (+ position value))
+          (= action "up")       (recur (rest moves) depth                   (- aim value) position)
+          (= action "down")     (recur (rest moves) depth                   (+ aim value) position))))))
+
+
 (def one (navigate input))
+(def two (navigate-aim input))
 
 (println "answers:")
 (println one)
+(println two)
