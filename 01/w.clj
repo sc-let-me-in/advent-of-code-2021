9c08737bbf73976ba20465731288d2d601572ca9
William Reed <wreed58@gmail.com>
2021-12-02 10:32:48 -0500

    day 1, part 2

--- 01/w.clj
+++ 01/w.clj
@@ -1,14 +1,28 @@
 (require '(clojure.string))
 
 (def input (clojure.string/split-lines (slurp *in*)))
+(def input-nums (map (fn [n-string] (Integer/parseInt n-string)) input))
 
-(def one
-  (second (reduce (fn [acc value-str]
+(defn sonar [nums]
+  (second (reduce (fn [acc value]
             (let [prev-val (first acc)
-                  sum (second acc)
-                  value (Integer/parseInt value-str)]
+                  sum (second acc)]
               (list value (+ sum (if (< prev-val value) 1 0)))))
           (list Integer/MAX_VALUE 0)
-          input)))
+          nums)))
 
+(defn convert-to-single [l]
+  (loop [items l
+         acc '()]
+    (cond
+      (< (count items) 3) (reverse acc)
+      :else (recur
+              (rest items)
+              (cons (reduce + (take 3 items)) acc)))))
+
+(def one (sonar input-nums))
+(def two (sonar (convert-to-single input-nums)))
+
+(println "answers:")
 (println one)
+(println two)
