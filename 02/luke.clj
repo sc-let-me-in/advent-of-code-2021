6ce77c16e126b9e50d90283e0e53fc824cde2945
Luke Novak <contact@nluken.com>
2021-12-02 23:10:42 -0500

    d2p2 also dirty soln

--- 02/luke.clj
+++ 02/luke.clj
@@ -2,22 +2,38 @@
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as str]))
 
-(defn mapOverFileLineSeq [func filename]
-  (map func (with-open [rdr (io/reader filename)] (doall (line-seq rdr)))))
+;; make this public- it could be useful
+(def map-file-line-seq (fn [func filename]
+                              (map func (with-open [rdr (io/reader filename)] (doall (line-seq rdr))))))
 
-(defn getFinalPos [commands currX currZ]
-  (if (empty? commands) [currX currZ]
-      (let [currCmd (first commands) restCmd (rest commands)]
+(defn calc-simple-final-pos [commands x z]
+  (if (empty? commands) [x z]
+      (let [cmd (first commands) rst (rest commands)]
         (cond
-         (= "forward" (first currCmd)) (getFinalPos restCmd (+ currX (Integer/parseInt (second currCmd))) currZ)
-         (= "up" (first currCmd)) (getFinalPos restCmd currX (- currZ (Integer/parseInt (second currCmd))))
-         (= "down" (first currCmd)) (getFinalPos restCmd currX (+ currZ (Integer/parseInt (second currCmd))))))))
+          (= "forward" (first cmd)) (calc-simple-final-pos rst (+ x (second cmd)) z)
+          (= "up" (first cmd)) (calc-simple-final-pos rst x (- z (second cmd)))
+          (= "down" (first cmd)) (calc-simple-final-pos rst x (+ z (second cmd)))))))
 
-(let [finalPos
-      (getFinalPos
-       (mapOverFileLineSeq
-        (fn [str] (str/split str #" ")) 
-        "/home/nluken/Documents/advent-of-code-2021/02/input.txt")
-       0 0)]
+(defn calc-aimed-final-pos [commands x z pitch]
+  (if (empty? commands) [x z]
+      (let [cmd (first commands) rst (rest commands)]
+        (cond
+          (= "forward" (first cmd)) (calc-aimed-final-pos rst
+                                                          (+ x (second cmd))
+                                                          (+ z (* (second cmd) pitch)) pitch)
+          (= "up" (first cmd)) (calc-aimed-final-pos rst x z
+                                                     (- pitch (second cmd)))
+          (= "down" (first cmd)) (calc-aimed-final-pos rst x z
+                                                       (+ pitch (second cmd)))))))
 
-(print (* (first finalPos) (second finalPos))))
+(let [commands (map-file-line-seq
+                (fn [str]
+                  (let [strCmd (str/split str #" ")]
+                    [(first strCmd) (Integer/parseInt (second strCmd))]))
+                "/home/nluken/Documents/advent-of-code-2021/02/input.txt")
+      simple-final-pos (calc-simple-final-pos commands 0 0)
+      aimed-final-pos (calc-aimed-final-pos commands 0 0 0)]
+
+  ;; answers
+  (print (* (first simple-final-pos) (second simple-final-pos)))
+  (print (* (first aimed-final-pos) (second aimed-final-pos))))
