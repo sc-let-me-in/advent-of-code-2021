88badf19b4fca9e7989925864397ba5e700e3b97
Luke Novak <contact@nluken.com>
2021-12-06 22:51:57 -0500

    tiny cleanup of pt1 + d3p2

--- src/aoc/03/luke.clj
+++ src/aoc/03/luke.clj
@@ -6,11 +6,7 @@
 
 ;; go through all rows. add 1 for 1, subtract 1 for 0. Positive = 1, negative = 0
 (defn find-common-sequence [rows]
-  (let [start-scores
-        (loop [row (first rows)
-               scores '()]
-          (if (empty? row) (reverse scores) (recur (rest row) (cons 0 scores))))]
-
+  (let [start-scores (repeat (count (first rows)) 0)]
     (loop [remaining rows
            scores start-scores]
       (if (empty? remaining)
@@ -23,10 +19,32 @@
         (recur (rest remaining)
                (loop [row (first remaining) old scores updated '()]
                  (if (empty? row) (reverse updated)
-                     (if (= 1 (first row))
-                       (recur (rest row) (rest old) (cons (+ 1 (first old)) updated))
-                       (recur (rest row) (rest old) (cons (- (first old) 1) updated))))))))))
+                     (recur (rest row) 
+                            (rest old) 
+                            (cons (+ (- (* 2 (first row)) 1) (first old)) 
+                                  updated)))))))))
 
+(defn bit-elimination [rows keep-fn]
+  (loop [remaining rows
+         pos 0]
+    (if (> (count remaining) 1)
+      (recur (let [most-common (find-common-sequence remaining)]
+               (loop [a remaining kept '()]
+                 (cond (empty? a) kept
+                       (keep-fn (first a) most-common pos) (recur (rest a) (cons (first a) kept))
+                       :else (recur (rest a) kept))))
+             (inc pos))
+      (first remaining))))
+
+(defn get-oxygen-reading [rows]
+  (bit-elimination rows (fn [row most-common pos]
+                          (= (nth most-common pos) (nth row pos)))))
+
+(defn get-scrubber-reading [rows]
+  (bit-elimination rows (fn [row most-common pos]
+                          (not (= (nth most-common pos) (nth row pos))))))
+
+;; might be useful to have this
 (def exp (fn [n pow]
            (loop [n n pow pow tot 1]
              (if (= 0 pow) tot
@@ -38,5 +56,11 @@
     (if (empty? remaining) tally
         (recur (rest remaining) (+ tally (* (exp 2 power) (first remaining))) (+ 1 power)))))
 
-(let [final-seq (find-common-sequence (input))]
-  (print (* (convert-to-dec final-seq) (convert-to-dec (map (fn [x] (- 1 x)) final-seq)))))
+(let [rows (input)
+      final-seq (find-common-sequence rows)
+      oxy-reading (get-oxygen-reading rows)
+      scrubber-reading (get-scrubber-reading rows)]
+  (println "Power Consumption Reading: "
+           (* (convert-to-dec final-seq) (convert-to-dec (map (fn [x] (- 1 x)) final-seq))))
+  (println "Oxygen Reading:" (convert-to-dec oxy-reading))
+  (println "CO2 Scrubber Reading:" (convert-to-dec scrubber-reading)))
